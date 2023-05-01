f85bc06cf78f9d5d97a724d936edaaa7811804ee
Inkl1ng <scottbot2018@gmail.com>
2023-04-30 18:11:27 -0700

    feat: add move() and draw() methods
    Not adding a constructor because there is nothing that would need to be passed to the Ball object.

--- src/ball.cpp
+++ src/ball.cpp
@@ -1,1 +1,14 @@
+#include "ball.hpp"
+#include "raylib.h"
+#include <cmath>
 
+void Ball::move() {
+    double radians { angle * 3.14159 * 180 };
+
+    xPos += speed * std::cos(radians);
+    yPos += speed * std::sin(radians);
+}
+
+void Ball::draw() {
+    DrawCircle(xPos, yPos, RADIUS, WHITE);
+}
--- src/ball.hpp
+++ src/ball.hpp
@@ -1,16 +1,22 @@
 #ifndef BALL_HPP
 #define BALL_HPP
 
+#include "raylib.h"
+
+
 class Ball {
 public:
-    Ball();
-
     void move();
     void draw();
 
 private:
     const int RADIUS { 25 };
-    const int SPEED { 20 };
+
+    int xPos { 500 };
+    int yPos { 400 };
+    double speed { 25 };
+    // angle in degrees
+    int angle { 180 };
 };
 
 #endif
