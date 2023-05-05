5a518664a9d1687ec1334ca43006c80de4ba73fa
Inkl1ng <scottbot2018@gmail.com>
2023-05-05 12:35:39 -0700

    initial commit
    New file for holding constants

---
+++ src/constants.hpp
@@ -0,0 +1,9 @@
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+namespace constants {
+    inline constexpr int WIDTH { 1000 }; // screen width
+    inline constexpr int HEIGHT { 800 }; // screen height
+};
+
+#endif
