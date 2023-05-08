13b65c4d9bedb297cca8189b80b621715947df20
Inkl1ng <scottbot2018@gmail.com>
2023-05-08 13:16:47 -0700

    Add POINTS_TO_WIN

--- src/constants.hpp
+++ src/constants.hpp
@@ -4,6 +4,7 @@
 namespace constants {
     inline constexpr int WIDTH { 1000 }; // screen width
     inline constexpr int HEIGHT { 800 }; // screen height
+    inline constexpr int POINTS_TO_WIN { 1 };
 };
 
 #endif
