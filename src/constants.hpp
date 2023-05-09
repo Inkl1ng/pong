118300c6be740207d784667b2a93aa0ce8140ae2
Inkl1ng <scottbot2018@gmail.com>
2023-05-08 20:38:44 -0700

    Add player x position constants
    moved from main.cpp

--- src/constants.hpp
+++ src/constants.hpp
@@ -5,6 +5,9 @@ namespace constants {
     inline constexpr int WIDTH { 1000 }; // screen width
     inline constexpr int HEIGHT { 800 }; // screen height
     inline constexpr int POINTS_TO_WIN { 1 };
+    const int player1XPos { 25 };
+    const int player2XPos { WIDTH - 25 };
+ 
 };
 
 #endif
