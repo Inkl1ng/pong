6232f2809aeb15301a354d3ecc25f5ae8ff76b13
Inkl1ng <scottbot2018@gmail.com>
2023-05-21 16:01:22 -0700

    Add drawTitleText function

--- src/text.cpp
+++ src/text.cpp
@@ -5,35 +5,38 @@
 #include "types.hpp"
 #include <string>
 
-namespace text {
-    void drawScore(Paddle& player_1, Paddle& player_2) {
-        DrawText(TextFormat("%i", player_1.getScore()),
-                 x_padding, y_padding,
-                 score_size, BLUE);
-        int player_2_text_width = MeasureText(TextFormat("%i", player_2.getScore()),
-                score_size);
-        DrawText(TextFormat("%i", player_2.getScore()),
-                 constants::width - player_2_text_width - x_padding, 
-                 y_padding,
-                 score_size, RED);
-    }
-
-    void drawWinText(GameStatus& game_status) {
-        std::string win_text {};
-        std::string replay_text { "Play again? (Press Y/N)" };
-        if (game_status == GameStatus::PLAYER_1_WIN) {
-            win_text = "Player 1 wins!";
-        } else {
-            win_text = "Player 2 wins!";
-        }
-
-        int win_text_width = MeasureText(win_text.c_str(), win_size);
-        DrawText(win_text.c_str(), (constants::width/2) - (win_text_width/2), 300,
-                 win_size, WHITE);
-       
-        int replay_text_width = MeasureText(replay_text.c_str(), win_size);
-        DrawText(replay_text.c_str(), (constants::width/2) - (replay_text_width/2),
-                 350, win_size, WHITE);
-    }
+void text::drawScore(Paddle& player_1, Paddle& player_2) {
+    DrawText(TextFormat("%i", player_1.getScore()),
+         x_padding, y_padding,
+             score_size, BLUE);
+    int player_2_text_width = MeasureText(TextFormat("%i", player_2.getScore()),
+            score_size);
+    DrawText(TextFormat("%i", player_2.getScore()),
+             constants::width - player_2_text_width - x_padding, 
+             y_padding,
+             score_size, RED);
 }
 
+void text::drawWinText(GameStatus& game_status) {
+    std::string win_text {};
+    std::string replay_text { "Play again? (Press Y/N)" };
+    if (game_status == GameStatus::PLAYER_1_WIN) {
+        win_text = "Player 1 wins!";
+    } else {
+        win_text = "Player 2 wins!";
+    }
+
+    int win_text_width = MeasureText(win_text.c_str(), win_size);
+    DrawText(win_text.c_str(), (constants::width/2) - (win_text_width/2), 300,
+             win_size, WHITE);
+   
+    int replay_text_width = MeasureText(replay_text.c_str(), win_size);
+    DrawText(replay_text.c_str(), (constants::width/2) - (replay_text_width/2),
+             350, win_size, WHITE);
+}
+
+void text::drawTitleText() {
+    const int title_x {
+        constants::width/2 - MeasureText(title_text, title_size)/2 };
+    DrawText(title_text, title_x, 400, title_size, WHITE);
+}
--- src/text.hpp
+++ src/text.hpp
@@ -2,17 +2,23 @@
 #define TEXT_HPP
 
 #include "paddle.hpp"
+#include "raylib.h"
 #include "types.hpp"
+#include "constants.hpp"
+#include <string_view>
 
 namespace text {
-    const int x_padding { 100 };
-    const int y_padding { 100 };
-    const int score_size { 100 }; 
-    const int win_size { 30 };
+    inline const int x_padding { 100 };
+    inline const int y_padding { 100 };
+    inline const int score_size { 100 }; 
+    inline const int win_size { 30 };
+
+    inline const char *title_text { "Pong!" }; 
+    inline const int title_size { 40 };
 
     void drawScore(Paddle& player_1, Paddle& player_2);
     void drawWinText(GameStatus& game_status);
-    GameStatus drawReplayButton(GameStatus& game_status);
+    void drawTitleText();
 }
 
 #endif
