8701f9a23ead4028164d9ba1620808841e6ac753
Inkl1ng <scottbot2018@gmail.com>
2023-05-21 16:56:52 -0700

    Move around variables and add comments

--- src/text.cpp
+++ src/text.cpp
@@ -7,31 +7,33 @@
 
 void text::drawScore(Paddle& player_1, Paddle& player_2) {
     DrawText(TextFormat("%i", player_1.getScore()),
-         x_padding, y_padding,
-             score_size, BLUE);
+            score_x_padding, score_y_padding,
+            score_size, BLUE);
+
+    // measure thickness of text to apply the correct offset to make
+    // the two displayed texts symmetrical
     int player_2_text_width = MeasureText(TextFormat("%i", player_2.getScore()),
             score_size);
     DrawText(TextFormat("%i", player_2.getScore()),
-             constants::width - player_2_text_width - x_padding, 
-             y_padding,
-             score_size, RED);
+            constants::width - player_2_text_width - score_x_padding, 
+            score_y_padding,
+            score_size, RED);
 }
 
 void text::drawWinText(GameStatus& game_status) {
-    std::string win_text {};
-    std::string replay_text { "Play again? (Press Y/N)" };
-    if (game_status == GameStatus::PLAYER_1_WIN) {
-        win_text = "Player 1 wins!";
-    } else {
-        win_text = "Player 2 wins!";
-    }
+    // check which player won and use the right text
+    const char* win_text {};
+    (game_status == GameStatus::PLAYER_1_WIN) ? win_text = "Player 1 wins!"
+            : win_text = "Player 2 wins!";
 
-    int win_text_width = MeasureText(win_text.c_str(), win_size);
-    DrawText(win_text.c_str(), (constants::width/2) - (win_text_width/2), 300,
+    // draw text for which player won
+    int win_text_width = MeasureText(win_text, win_size);
+    DrawText(win_text, (constants::width/2) - (win_text_width/2), 300,
              win_size, WHITE);
    
-    int replay_text_width = MeasureText(replay_text.c_str(), win_size);
-    DrawText(replay_text.c_str(), (constants::width/2) - (replay_text_width/2),
+    // draw text for replay promt
+    int replay_text_width = MeasureText(replay_text, win_size);
+    DrawText(replay_text, (constants::width/2) - (replay_text_width/2),
              350, win_size, WHITE);
 }
 
@@ -40,16 +42,13 @@ void text::drawTitleText() {
     const int right_width { MeasureText(title_text_right, title_size/2) };
     // draw left side of pong text
     const int title_left_x { constants::width/2
-        - MeasureText(title_text_left, title_size)/2 - right_width };
+            - MeasureText(title_text_left, title_size)/2 - right_width };
     DrawText(title_text_left, title_left_x, title_y, title_size, BLUE);
 
-    // draw right side of pong text
+    // measure and draw right side of pong text
     // subtract 18 to maintain correct spacing between letters, random
     // were guessed and checked until the pixel spacing was correct
     const int title_right_x { constants::width/2
-        - MeasureText(title_text_right, title_size)/2 + right_width + 18 };
+            - MeasureText(title_text_right, title_size)/2 + right_width + 18 };
     DrawText(title_text_right, title_right_x, title_y, title_size, RED);
-    // const int test { constants::width/2
-    //     - MeasureText("Pong", title_size)/2};
-    // DrawText("Pong", test, title_y, title_size, WHITE);
 }
--- src/text.hpp
+++ src/text.hpp
@@ -8,15 +8,19 @@
 #include <string_view>
 
 namespace text {
-    inline const int x_padding { 100 };
-    inline const int y_padding { 100 };
+    // x distance from score number to boarder
+    inline const int score_x_padding { 100 };
+    // y distance from score number to boarder
+    inline const int score_y_padding { 100 };
+
     inline const int score_size { 100 }; 
     inline const int win_size { 30 };
-
-    inline const char *title_text_left { "Po" };
-    inline const char *title_text_right { "ng" };
     inline const int title_size { 120 };
     inline const int title_y { 100 };
+
+    inline const char *replay_text { "Play again? (Y/N)" };
+    inline const char *title_text_left { "Po" };
+    inline const char *title_text_right { "ng" };
 
     void drawScore(Paddle& player_1, Paddle& player_2);
     void drawWinText(GameStatus& game_status);
