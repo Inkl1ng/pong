7e4857604ea79b959086d781008a094be6d2fb77
Inkl1ng <scottbot2018@gmail.com>
2023-05-21 17:39:20 -0700

    Add documentation

--- src/constants.hpp
+++ src/constants.hpp
@@ -4,8 +4,11 @@
 namespace constants {
     inline constexpr int width { 1000 }; // screen width
     inline constexpr int height { 800 }; // screen height
-    inline constexpr int points_to_win { 1 };
+    inline constexpr int points_to_win { 1 }; // points needed to win
+
+    /// x-position of player 1's paddle
     const int player_1_x_pos { 25 };
+    /// x-position of player 2's paddle
     const int player_2_x_pos { width - 25 };
 };
 
